525d02cef026b12b04c8c49a250ff1dde8bbceac
dusk <dusklinux@users.noreply.github.com>
2026-08-06 19:07:02 +0530

    o system upgrade

--- user_scripts/update_dusky/update_dusky.sh
+++ user_scripts/update_dusky/update_dusky.sh
@@ -204,7 +204,7 @@ declare -ra UPDATE_SEQUENCE=(
 
 
 #================= CUSTOM=====================
-    "S | 060_package_installation.py -p updater_packages"
+    "S | 060_package_installation.py -p updater_packages --no-upgrade"
     "U | paru_packages.sh"
 #================= Scripts =====================
 
